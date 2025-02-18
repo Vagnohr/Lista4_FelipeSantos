aa01bf69301f4c852662ec981faad6607b5d2220
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-17 21:27:44 -0300

    up

--- atividade5.py
+++ atividade5.py
@@ -1,2 +1,8 @@
 #pessa ao usuario para inserir um número que deseja a tabuada e em seguida exibir a tabuada do até 10
 numero=int(input("Insira o número do qual deseja a tabuada: "))
+i = 1
+x = 10
+for x in i:
+    tabuada=numero*i
+    print(numero,"x",i,"=",tabuada)
+    i += 1
--- atividade9.py
+++ atividade9.py
@@ -1,1 +1,1 @@
-#
+#fassa um programa que pergunte em qual direção o usuario deseja contar(C/c, A/a) se ele selecionar para cima, pessa um numero superior, e conte de 1 até esse número. se ele selecionar para baixo pessa a ele para inserir um número abaixo de 20 e em seguida faça contagem regressiva de 20 até esse número, se ele inserir algum valor que seja diferente, exiba a mensagem "diressão invalida"
