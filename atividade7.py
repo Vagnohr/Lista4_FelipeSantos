f66e1ed66a89cd58f52247790e001814de97a02f
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-17 21:40:18 -0300

    Update atividade7.py

--- atividade7.py
+++ atividade7.py
@@ -1,10 +1,9 @@
 #pessa ao usuario para inserir seu nome e um número. se o número for menor que 10, exiba o nome dele esse número de vezes, caso contrario exiba a mensagem "número muito alto"
 nome=input("Insira o seu nome: ")
 numero=input("Insira um número: ")
-i=0
 if int(numero) < 10:
     for i in enumerate(numero):
         print(nome)
-        i = i+1
+        i +=1
 else:
     print("número muito alto")
