82c6420a978b86b914ebc86cf9fc52025ce96062
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 19:07:48 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,6 +1,5 @@
 #pessa ao usuario para insirir o seu nome, e em seguida exibir o seu nome 3 vezes
 nome=input("Insira o seu nome: ")
-x=[1,2,3]
-for i in x:
+x=3
+for i in range(x):
     print(nome)
-    i+=1
