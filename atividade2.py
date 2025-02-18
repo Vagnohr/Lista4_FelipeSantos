3bddb362bdf8de097b3cfb9590cfd8083a7ce267
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 19:06:28 -0300

    Update atividade2.py

--- atividade2.py
+++ atividade2.py
@@ -1,1 +1,5 @@
 #fassa um programa que solicite ao usuario para digitar o seu nome e um número qualquer e em seguida exiba o seu nome varias vezes de acordo com o número que digitou
+nome=input("Insira o seu nome: ")
+num=int(input("Insra quantidade de repetições: "))
+for i in range(num):
+    print(nome)
