752099d3323c692eb379fed394cfd8a101064337
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 19:05:23 -0300

    Update atividade4.py

--- atividade4.py
+++ atividade4.py
@@ -1,1 +1,6 @@
 #escreva um programa para pedir um número e em seguida o nome. exiba o nome(uma letra de cada vez em cada linha) e repita isso para o número de vezes que ele digitou
+nome=input("Insira o seu nome: ")
+num=int(input("Insira número de repetições: "))
+for i in range(num):
+    for x in nome:
+        print(x)
