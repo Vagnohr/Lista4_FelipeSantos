96aecdd6bee6d9e6d4879dbf8c7cb6e1f3359254
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 19:03:36 -0300

    Update atividade6.py

--- atividade6.py
+++ atividade6.py
@@ -1,1 +1,8 @@
 #pessa um número abaixo de 50 e em seguida fassa um contagem regressiva até esse número, certificando-se desmotrar o número que eles inseriram na saida
+num=int(input("Insira um número abaixo de 50: "))
+if num<50:
+    for i in range(num,51):
+        print(i)
+        print(num)
+else:
+    print("Número invalido")
