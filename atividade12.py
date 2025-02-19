14587aa3d25a8db23da246261ae9cb4c4fd05888
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 21:43:23 -0300

    Update atividade12.py

--- atividade12.py
+++ atividade12.py
@@ -6,13 +6,18 @@ for i in range(quantidade):
     horarios.append(horario)
 print(horarios)
 cliente=input("Insira qual horario deseja ocupar: ")
-tomado=horarios.pop(cliente)
+horarios.remove(cliente)
+tomado.append(cliente)
 outro=input("mais algum cliente deseja marcar hora? ")
-if "S" in outro or "s" in outro:
-    print("horarios livres:",horarios,"horarios ocupados:",tomado)
-    cliente=int(input("Insira qual horario deseja ocupar: "))
-    tomado.append(cliente)
-    outro=input("mais algum cliente deseja marcar hora? ")
-elif "N" or outro or "n" in outro:
-    print("Obrigado por vir")
+for i in range(len(horarios)):
+    if "S" in outro or "s" in outro:
+        print("horarios livres:",horarios,"horarios ocupados:",tomado)
+        cliente=input("Insira qual horario deseja ocupar: ")
+        horarios.remove(cliente)
+        tomado.append(cliente)
+        outro=input("mais algum cliente deseja marcar hora? ")
+    elif "N" or outro or "n" in outro:
+        print("Obrigado por vir")
+else:
+    print("todos os horarios marcados")
 print("Felipe Bortoluzzi dos Santos")
