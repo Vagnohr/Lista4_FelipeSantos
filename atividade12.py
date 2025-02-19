82d1e2647f408451eb4544bd928862b9858d7c70
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 21:33:33 -0300

    Update atividade12.py

--- atividade12.py
+++ atividade12.py
@@ -1,16 +1,17 @@
 quantidade=int(input("Insira quantos horarios deseja adicionar: "))
 horarios=[]
+tomado=[]
 for i in range(quantidade):
     horario=input("Insira quais horarios estão disponiveis: ")
     horarios.append(horario)
 print(horarios)
 cliente=input("Insira qual horario deseja ocupar: ")
-tomado=horarios.remove(cliente)
+tomado=horarios.pop(cliente)
 outro=input("mais algum cliente deseja marcar hora? ")
 if "S" in outro or "s" in outro:
-    print(horarios)
-    cliente=input("Insira qual horario deseja ocupar: ")
-    tomado=horarios.remove(cliente)
+    print("horarios livres:",horarios,"horarios ocupados:",tomado)
+    cliente=int(input("Insira qual horario deseja ocupar: "))
+    tomado.append(cliente)
     outro=input("mais algum cliente deseja marcar hora? ")
 elif "N" or outro or "n" in outro:
     print("Obrigado por vir")
